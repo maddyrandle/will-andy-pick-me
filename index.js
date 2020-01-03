1b371cdcf2cb3341f31897f69170af0a95833428
Madison Randle <madisonrandle25@gmail.com>
2020-01-02 18:35:41 -0700

    Change variable names and add styling when clear button is active

--- index.js
+++ index.js
@@ -1,49 +1,45 @@
-var userQuestion = document.querySelector("input");
-var userCard = document.querySelector(".user-card");
-var getAnswer = document.querySelector(".get-answer-btn");
+var questionInput = document.querySelector("input");
+var messageCard = document.querySelector(".user-card");
+var answerBtn = document.querySelector(".get-answer-btn");
 var clearBtn = document.querySelector(".clear-btn");
-var eightBall = document.querySelector(".eight-ball");
+var eightBallImage = document.querySelector(".eight-ball");
 
-clearBtn.addEventListener("click", resetQAndA);
-getAnswer.addEventListener("click", replaceImage);
+clearBtn.addEventListener("click", showEightBall);
+answerBtn.addEventListener("click", replaceImageWithAnswer);
 
 
-function replaceImage() {
-  eightBall.classList.add("hidden");
-  userCard.classList.remove("hidden");
+function replaceImageWithAnswer() {
+  eightBallImage.classList.add("hidden");
+  messageCard.classList.remove("hidden");
+  clearBtn.classList.add("active-clear-btn");
   addCard();
-  clearForm();
+  showDefaultInputMsg();
 }
 
-function clearForm() {
-  userQuestion.value = "Ask your question here!";
+function showDefaultInputMsg() {
+  questionInput.value = "Ask your question here!";
 }
 
-function resetQAndA() {
-  removeCard();
-  eightBall.classList.remove("hidden");
-  userCard.classList.add("hidden");
+function showEightBall() {
+  // removeCard();
+  eightBallImage.classList.remove("hidden");
+  messageCard.classList.add("hidden");
+  clearBtn.classList.remove("active-clear-btn");
 }
 
-function removeCard() {
-  if (event.target.classList.contains("clear-btn")) {
-  }
-};
+// function removeCard() {
+//   if (event.target.classList.contains("clear-btn")) {
+//   }
+// };
 
 function addCard() {
   var answers = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes - definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."]
   var randomize = Math.floor(Math.random()*answers.length);
   var show = answers[randomize];
-  userCard.insertAdjacentHTML("afterbegin", `
-    <p class="user-question">"${userQuestion.value}"</p>
+  messageCard.insertAdjacentHTML("afterbegin", `
+    <p class="user-question">"${questionInput.value}"</p>
     <p class="user-answer">${show}</p>
   `);
 };
 
-// function showRandomAnswer() {
-//   var answers = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes - definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."]
-//   var randomize = Math.floor(Math.random()*answers.length);
-//   var show = answers[randomize];
-//
-// }
 //
