e3e0df44bc32dff6bb5431da9dea035456196279
Madison Randle <madisonrandle25@gmail.com>
2020-01-02 19:01:56 -0700

    Update var and function names and disable/enable clear button

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var questionInput = document.querySelector("input");
-var messageCard = document.querySelector(".user-card");
+var messageCard = document.querySelector(".message-card");
 var answerBtn = document.querySelector(".get-answer-btn");
 var clearBtn = document.querySelector(".clear-btn");
 var eightBallImage = document.querySelector(".eight-ball");
@@ -12,11 +12,11 @@ function replaceImageWithAnswer() {
   eightBallImage.classList.add("hidden");
   messageCard.classList.remove("hidden");
   clearBtn.classList.add("active-clear-btn");
-  addCard();
-  showDefaultInputMsg();
+  showMessage();
+  showDefaultInputText();
 }
 
-function showDefaultInputMsg() {
+function showDefaultInputText() {
   questionInput.value = "Ask your question here!";
 }
 
@@ -25,6 +25,7 @@ function showEightBall() {
   eightBallImage.classList.remove("hidden");
   messageCard.classList.add("hidden");
   clearBtn.classList.remove("active-clear-btn");
+  clearBtn.disabled = true;
 }
 
 // function removeCard() {
@@ -32,14 +33,15 @@ function showEightBall() {
 //   }
 // };
 
-function addCard() {
-  var answers = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes - definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."]
-  var randomize = Math.floor(Math.random()*answers.length);
-  var show = answers[randomize];
+function showMessage() {
+  var eightBallAnswers = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes - definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."]
+  var randomize = Math.floor(Math.random()*eightBallAnswers.length);
+  var randomAnswer = eightBallAnswers[randomize];
   messageCard.insertAdjacentHTML("afterbegin", `
-    <p class="user-question">"${questionInput.value}"</p>
-    <p class="user-answer">${show}</p>
+    <p class="question">"${questionInput.value}"</p>
+    <p class="random-answer">${randomAnswer}</p>
   `);
+  clearBtn.disabled = false;
 };
 
 //
