bf6a1af823a7f73452e7a71435b76a67404beb7e
Madison Randle <madisonrandle25@gmail.com>
2020-01-02 19:03:54 -0700

    Remove unused function

--- index.js
+++ index.js
@@ -21,17 +21,13 @@ function showDefaultInputText() {
 }
 
 function showEightBall() {
-  // removeCard();
   eightBallImage.classList.remove("hidden");
   messageCard.classList.add("hidden");
   clearBtn.classList.remove("active-clear-btn");
   clearBtn.disabled = true;
 }
 
-// function removeCard() {
-//   if (event.target.classList.contains("clear-btn")) {
-//   }
-// };
+
 
 function showMessage() {
   var eightBallAnswers = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes - definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."]
